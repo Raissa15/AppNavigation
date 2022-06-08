2edc5321c6a62b2f0baadfdd8114008ace23cda8
Raissa15 <raissa.ramos4@etec.sp.gov.br>
2022-06-08 14:36:30 -0300

    App troca de tela

---
+++ app/src/main/java/com/example/appnavigation/fragment_first.kt
@@ -0,0 +1,34 @@
+package com.example.appnavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [fragment_first.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class fragment_first : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_first, container, false)
+        view.setOnClickListener{Navigation.findNavController(view).navigate(R.id.action_fragment_first_to_fragment_second)}
+        return view
+    }
+
+
+}
